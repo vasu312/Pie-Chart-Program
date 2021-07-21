5880a6575bcda6667c1b3344b6a0a8e19b15aa36
Vasudevan <87578010+vasu312@users.noreply.github.com>
2021-07-21 23:10:21 +0530

    Update pie.py

--- pie.py
+++ pie.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-y=np.array([35,25,20,10])
-datas=["Mobile","TV","Laptop","Ipod"]
+# Data to plot
+labels = 'Python', 'C++', 'Ruby', 'Java'
+sizes = [215, 130, 245, 210]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)  # explode 1st slice
 
-plt.pie(y, labels=datas,shadow=True)
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
 plt.show()
